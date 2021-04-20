74845c7c4f1f53e7c360765e45726365db5ae4ff
emsesc <emseschen@gmail.com>
2021-04-19 20:26:52 -0400

    Edit tests/update workflows

--- .bit/tests/test.secrets2.js
+++ .bit/tests/test.secrets2.js
@@ -1,9 +1,9 @@
-  let key = undefined
+let key = undefined
 let endpoint = undefined
 
 key = process.env.SUBSCRIPTION_KEY
 endpoint = process.env.API_ENDPOINT
 
-if (endpoint == null || key == null) {
+if (endpoint[0] == null || key[0] == null) {
     throw new Error("You have not added your secrets!");
 }
