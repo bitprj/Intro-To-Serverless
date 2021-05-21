42d3c867df48b296ddf47fa5213dbc81afddd9ea
emsesc <emseschen@gmail.com>
2021-05-21 18:21:00 -0400

    week 1 test udpate

---
+++ .bit/tests/test.secret.js
@@ -0,0 +1,21 @@
+let endpoint = undefined
+
+endpoint = process.env.SECRET
+
+if (endpoint[0] != "h") {
+   throw new Error("You have not added your function url as a secret!");
+}
+
+// const fs = require('fs') //get the methods in the fs package
+
+//if you wanna add more files, just put a comma after the filename (array)
+// const commit_file = ['package.json']
+
+// for(var i = 0; i < commit_file.length; i++) {
+//     var a = commit_file[i];
+//     fs.access(commit_file[i], err => {
+//         if (err) {
+//           throw new Error("You did not commit '" + a + "'")
+//         }
+//     })
+// }
--- .bit/tests/test.secrets2.js
+++
@@ -1,9 +0,0 @@
-let key = undefined
-let endpoint = undefined
-
-key = process.env.SUBSCRIPTION_KEY
-endpoint = process.env.API_ENDPOINT
-
-if (endpoint[0] == null || key[0] == null) {
-    throw new Error("You have not added your secrets!");
-}
