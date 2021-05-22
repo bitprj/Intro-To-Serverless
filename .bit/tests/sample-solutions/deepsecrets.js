aa77ce69d651aa824977001bf41ee9c69c49d7a7
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-05-22 14:46:05 -0400

    Update deepsecrets.js

--- .bit/tests/sample-solutions/deepsecrets.js
+++ .bit/tests/sample-solutions/deepsecrets.js
@@ -1,40 +1,66 @@
+const querystring = require('querystring');
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+// npm install @azure/cosmos
+
+const config = {
+  endpoint: process.env.ENDPOINT,
+  key: process.env.KEY,
+  databaseId: "SecretStorer",
+  containerId: "secrets",
+  partitionKey: {kind: "Hash", paths: ["/secrets"]}
+};
+
+async function create(client, databaseId, containerId) {
+   const { database } = await client.databases.createIfNotExists({
+      id: config.databaseId
+   });
+
+   const { container } = await client
+      .database(config.databaseId)
+      .containers.createIfNotExists(
+         { id: config.containerId, key: config.partitionKey },
+         { offerThroughput: 400 }
+      );
+   }
+
+async function createDocument(newItem) {
+    var { endpoint, key, databaseId, containerId } = config;
+    const client = new CosmosClient({endpoint, key});
+    const database = client.database(databaseId);
+    const container = database.container(containerId);
+    await create(client, databaseId, containerId);
+
+    const querySpec = {
+        query: "SELECT * from c"
+    };
+
+// read all items in the Items container
+    const { resources: items } = await container.items
+        .query(querySpec)
+        .fetchAll();
+
+    const {resource: createdItem} = await container.items.create(newItem);
+    return items
+}
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    function printTime() {
-        var date = new Date();
-        var hour = date.getHours();
-        var min = date.getMinutes();
-        var sec = date.getSeconds();
-        hour = updateTime(hour) + 7;
-        min = updateTime(min);
-        sec = updateTime(sec);
-        if (hour < 12) {
-            tday = "AM";
-        }
-        else {
-            tday = "PM"
-            if (hour != 12) {
-            hour -= 12
-            }
-        }
 
-        var clock = hour + " : " + min + " : " + sec + " " + tday
+    const queryObject = querystring.parse(req.body);
+    message = queryObject.Body;
+    let document = {"message":message}
 
-        function updateTime(time) {
-        if (time < 10) {
-            return "0" + time;
-        }
-        else {
-            return time;
-        }
-        }
-        return clock;
-    }
- 
-    const responseMessage = "Hello! The current date is: " + printTime()
+    let items = await createDocument(document)
+    context.log(items)
+    var random_value = Math.floor(items.length * Math.random())
+
+    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[random_value].message)}`
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
 }
+
+// https://www.twilio.com/docs/sms/quickstart/node
+// https://www.neilwithdata.com/azure-functions-post-body-js
