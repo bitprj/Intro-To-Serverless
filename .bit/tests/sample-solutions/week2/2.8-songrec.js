d7a3ddda6d571a9b747e2fd3307c10446627bc41
emsesc <emseschen@gmail.com>
2021-06-03 15:43:02 -0400

    update week 2 second project

--- .bit/tests/sample-solutions/week2/2.8-songrec.js
+++ .bit/tests/sample-solutions/week2/2.8-songrec.js
@@ -1,22 +1,16 @@
 const querystring = require('querystring');
 var fetch = require("node-fetch");
 
-const accountSid = "***";
-const authToken = "***";
-const client = require('twilio')(accountSid, authToken);
-// https://www.twilio.com/docs/voice/quickstart/node
-
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-// https://www.twilio.com/docs/voice/twiml/play
-
 async function analyzeImage(img){
     const subscriptionKey = process.env['subscriptionkey'];
     const uriBase = process.env['endpoint'] + '/face/v1.0/detect';
 	// env variables (similar to .gitignore/.env file) to not expose personal info
 
+    // twilio phone-numbers:update "+15869913930" --sms-url="https://twlio-test.azurewebsites.net/api/HttpTrigger1?code=P0wvX4Wrv4iTCT8zw6r356rKLTDzNLC5g5OBfrvoIiJjdUr3jNzr3A=="
+
     let params = new URLSearchParams({
-	'returnFaceId': 'true',
-	'returnFaceAttributes': ['age']
+        'returnFaceId': 'true',
+        'returnFaceAttributes': ['age']
     })
 
     // making the post request
@@ -43,6 +37,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const queryObject = querystring.parse(req.body);
+    context.log(req.body)
     context.log(queryObject)
     url = queryObject.MediaUrl0;
     phone_number = queryObject.From;
@@ -60,6 +55,7 @@ module.exports = async function (context, req) {
     context.log(result)
 
     let age = result[0].faceAttributes.age
+    context.log(age)
     if (age > 5 && age < 25) {
         id = "GenZ"
     } else if (age > 24 && age < 41) {
@@ -69,27 +65,19 @@ module.exports = async function (context, req) {
     } else if (age > 56 && age < 76) {
         id = "BabyBoomers"
     } else {
-        id = "nothing"
+        id = "Unknown"
     }
+    context.log(id)
 
-    const songs = {"GenZ":"https://github.com/bitprj/Intro-To-Serverless/blob/main/.bit/music/SALES%20-%20Chinese%20new%20year.mp3", "GenY":"https://github.com/bitprj/Intro-To-Serverless/blob/main/.bit/music/Spice%20Girls%20-%20Wannabe.mp3", "GenX":"https://github.com/bitprj/Intro-To-Serverless/blob/main/.bit/music/Golden%20Earring%20-%20Radar%20Love%20(1973)%20HD%200815007.mp3", "BabyBoomers":"https://github.com/bitprj/Intro-To-Serverless/blob/main/.bit/music/Elvis%20Presley%20-%20Jailhouse%20Rock%20(Music%20Video).mp3", "nothing":"https://api.twilio.com/cowbell.mp3"}
+    const songs = {"GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+                   "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+                   "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+                   "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+                   "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
     let song = songs[id]
-    context.log(song)
-    const response = new VoiceResponse();
-    response.play({
-        loop: 3
-    }, song);
-
-    client.calls
-      .create({
-         twiml: response,
-         to: phone_number,
-         from: '+15869913930' // your own twilio number
-       })
-      .then(call => console.log(call.sid));
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: `We guessed you're this age: ${id}! We'll be calling you soon, hang tight.`
+        body: `We guessed you're part of this generation: ${id}! Happy listening! ${song}`
     };
 }
