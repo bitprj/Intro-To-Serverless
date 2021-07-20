4cf3ed396f7591f0888aefdfca5db8a3a0b82669
Ganning Xu <ganningxu@gmail.com>
2021-07-20 09:40:20 -0400

    Update script.js

--- .bit/tests/sample-solutions/week4/4.7/script.js
+++ .bit/tests/sample-solutions/week4/4.7/script.js
@@ -1,4 +1,8 @@
-async function y1k3s() {
+
+let submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
     let url = `${AZURE_URL}&name1=${document.getElementById("name1").value}&name2=${document.getElementById("name2").value}&name3=${document.getElementById("name3").value}&name4=${document.getElementById("name4").value}`
     let resp = await fetch(url, {
             method: 'GET'
@@ -10,4 +14,4 @@ async function y1k3s() {
         document.getElementById("image2").src = "data:image/png;base64," + result.cat2
         document.getElementById("image3").src = "data:image/png;base64," + result.cat3
         document.getElementById("image4").src = "data:image/png;base64," + result.cat4
-  }
+});
