07287ab8b5373a956f7365c14bba1b708b2e7c33
Ganning Xu <ganningxu@gmail.com>
2021-07-27 15:39:43 -0400

    Update test.1.2.js

--- .bit/tests/test.1.2.js
+++ .bit/tests/test.1.2.js
@@ -3,20 +3,24 @@ const functions = require('./functions');
 const user = args['user'];
 const repo = args['repo'];
 
-try { hello = require('./../../week1/helloworld.js') }
-catch(e) {
-    functions.throwError("Searching for 'helloworld.js'... file cannot be found", user, repo)
-    console.error("Searching for 'helloworld.js'... file cannot be found");
-    process.exit(1)
+async function main() {
+    try { hello = require('./../../week1/helloworld.js') }
+    catch (e) {
+        await functions.throwError("Searching for 'helloworld.js'... file cannot be found", user, repo)
+        console.error("Searching for 'helloworld.js'... file cannot be found");
+        process.exit(1)
+    }
+
+    let helloworld = hello()
+    let test_output = "Hello World"
+
+    if (helloworld != test_output) {
+        await functions.throwError(`Got: '${helloworld}', was expecting: '${test_output}'.`, user, repo)
+        console.error(`Got: "${helloworld}", was expecting: "${test_output}".`)
+        process.exit(1)
+    }
+
+    console.info("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
 }
 
-let helloworld = hello()
-let test_output = "Hello World"
-
-if(helloworld != test_output){
-    functions.throwError(`Got: '${helloworld}', was expecting: '${test_output}'.`, user, repo)
-    console.error(`Got: "${helloworld}", was expecting: "${test_output}".`)
-    process.exit(1)
-}
-
-console.info("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
+main();
