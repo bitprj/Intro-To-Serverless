78aef4af9d24e1e9d246c47219716e081ecb2e02
Ganning Xu <ganningxu@gmail.com>
2021-07-27 15:22:37 -0400

    Update test.1.2.js

--- .bit/tests/test.1.2.js
+++ .bit/tests/test.1.2.js
@@ -1,8 +1,12 @@
-let hello = undefined
+const args = require('minimist')(process.argv.slice(2))
+const functions = require('./functions');
+const user = args['user'];
+const repo = args['repo'];
 
 try { hello = require('./../../week1/helloworld.js') }
 catch(e) {
-    console.error("Searching for \"helloworld.js\"... file cannot be found");
+    functions.throwError("Searching for 'helloworld.js'... file cannot be found", user, repo)
+    console.error("Searching for 'helloworld.js'... file cannot be found");
     process.exit(1)
 }
 
@@ -10,6 +14,7 @@ let helloworld = hello()
 let test_output = "Hello World"
 
 if(helloworld != test_output){
+    functions.throwError(`Got: '${helloworld}', was expecting: '${test_output}'.`, user, repo)
     console.error(`Got: "${helloworld}", was expecting: "${test_output}".`)
     process.exit(1)
 }
