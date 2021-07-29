68cb8505a201aea406ea4d14962282a1fe551988
Ganning Xu <ganningxu@gmail.com>
2021-07-29 12:52:58 -0400

    Update test.1.4.js

--- .bit/tests/test.1.4.js
+++ .bit/tests/test.1.4.js
@@ -46,30 +46,3 @@ try {
     console.error("You have not added your function url as a secret!");
     process.exit(1)
 }
-
-
-
-
-
-
-async function main() {
-    try { hello = require('./../../week1/helloworld.js') }
-    catch (e) {
-        await functions.throwError("Searching for 'helloworld.js'... file cannot be found", user, repo)
-        console.log("Searching for 'helloworld.js'... file cannot be found");
-        process.exit(1)
-    }
-
-    let helloworld = hello()
-    let test_output = "Hello World"
-
-    if (helloworld != test_output) {
-        await functions.throwError(`Got: '${helloworld}', was expecting: '${test_output}'.`, user, repo)
-        console.log(`Got: "${helloworld}", was expecting: "${test_output}".`)
-        process.exit(1)
-    }
-
-    console.info("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
-}
-
-main();
