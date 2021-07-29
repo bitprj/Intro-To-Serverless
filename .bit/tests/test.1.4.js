6b6a6b1d72b21549d91d06da6fdc167901abfda2
Ganning Xu <ganningxu@gmail.com>
2021-07-29 12:51:06 -0400

    Update test.1.4.js

--- .bit/tests/test.1.4.js
+++ .bit/tests/test.1.4.js
@@ -1,6 +1,9 @@
-let uri = undefined
+let uri;
 const fetch = require('node-fetch');
-const functions = require('./functions.js')
+const args = require('minimist')(process.argv.slice(2))
+const functions = require('./functions');
+const user = args['user'];
+const repo = args['repo'];
 
 uri = process.env.HACKERVOICE_ENDPOINT;
 
@@ -29,11 +32,44 @@ try {
         if (data == test1) {     
             console.info("Yay! 🎉 We got: " + JSON.stringify(data) + ", which matches our input.")
         } else {
+            await functions.throwError("We got this " + JSON.stringify(data) + ". We should have gotten our input, 'fifiiscool' ... Try again!", user, repo)
             console.error("We got this " + JSON.stringify(data) + ". We should have gotten our input, 'fifiiscool' ... Try again!")
             process.exit(1)
         }
-    })().catch( e => { console.error("Try again! We got this error when trying to make a request: " + e); process.exit(1) })
+    })().catch( e => { 
+        console.error("Try again! We got this error when trying to make a request: " + e); 
+        await functions.throwError("Try again! We got this error when trying to make a request: " + e, user, repo)
+        process.exit(1) 
+    })
 } catch (e) {
+    await functions.throwError("You have not added your function url as a secret!", user, repo)
     console.error("You have not added your function url as a secret!");
     process.exit(1)
 }
+
+
+
+
+
+
+async function main() {
+    try { hello = require('./../../week1/helloworld.js') }
+    catch (e) {
+        await functions.throwError("Searching for 'helloworld.js'... file cannot be found", user, repo)
+        console.log("Searching for 'helloworld.js'... file cannot be found");
+        process.exit(1)
+    }
+
+    let helloworld = hello()
+    let test_output = "Hello World"
+
+    if (helloworld != test_output) {
+        await functions.throwError(`Got: '${helloworld}', was expecting: '${test_output}'.`, user, repo)
+        console.log(`Got: "${helloworld}", was expecting: "${test_output}".`)
+        process.exit(1)
+    }
+
+    console.info("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
+}
+
+main();
