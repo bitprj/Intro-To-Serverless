8a87dcdfa5cbfbbb527b3fa1cde0aa299bee3347
Ganning Xu <ganningxu@gmail.com>
2021-08-04 13:50:21 -0400

    Update 4.1.spec.js

--- cypress/integration/4.1.spec.js
+++ cypress/integration/4.1.spec.js
@@ -1,8 +1,14 @@
-describe('Testing Bunnimage', () => {
-    it('Testing Week 4 Step 1', () => {
-        cy.visit('bunnimage/index.html')
-        cy.get('input[type="text"]').type('console.log("hi yall")') 
-        cy.get('input[type="button"]').click()
-        cy.get('#output').contains('console.log("hi yall")❤️')
+try {
+    describe('Testing Bunnimage', () => {
+        it('Testing Week 4 Step 1', () => {
+            cy.visit('bunnimage/index.html')
+            cy.get('input[type="text"]').type('console.log("hi yall")') 
+            cy.get('input[type="button"]').click()
+            cy.get('#output').contains('console.log("hi yall")❤️')
+        })
     })
-})
+}
+catch (e) {
+    console.error("CUSTOM ERROR MSG: " + e)
+ 
+}
