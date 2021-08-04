7a08f030bd59dca9c913ccdb86ea8936e2846426
Ganning Xu <ganningxu@gmail.com>
2021-08-04 10:30:41 -0400

    Update test.1.6.js

--- .bit/tests/test.1.6.js
+++ .bit/tests/test.1.6.js
@@ -1,42 +1,46 @@
-// const fetch = require('node-fetch');
-// const functions = require('./functions.js')
-// uri = process.env.TWOCATZ_ENDPOINT
+const fetch = require('node-fetch');
+const args = require('minimist')(process.argv.slice(2))
+const functions = require('./functions');
+const user = args['user'];
+const repo = args['repo'];
 
-// functions.checkSecret(uri, "TWOCATZ_ENDPOINT")
+let uri = process.env.TWOCATZ_ENDPOINT
 
-// //if you wanna add more files, just put a comma after the filename (array)
-// const commit_file = ['twocatz/index.js']
+async function main() {
+    try {
+        functions.checkSecret(uri, "TWOCATZ_ENDPOINT")
 
-// functions.checkCommit(commit_file)
+        //if you wanna add more files, just put a comma after the filename (array)
+        const commit_file = ['twocatz/index.js']
 
-// try {
-//   (async () => {
-//       const resp = await fetch(uri, {
-//           method: 'GET'
-//       });
-//       var data = await resp.json()
-//       let test = JSON.stringify(data)
+        functions.checkCommit(commit_file)
 
-//       functions.validateResponseStatus(resp, uri)
-      
-//       if (test.length < 3) {
-//           console.error("No response... Try again!")
-//           process.exit(1)
-//       }
+        const resp = await fetch(uri, {
+            method: 'GET'
+        });
+        var data = await resp.json()
+        let test = JSON.stringify(data)
 
-//       try {
-//           var catimage = test;
-//           var newCat = Buffer.from(catimage, 'base64').toString('ascii')
-//           console.info("Yay! 🎉 We got your cat picture 🐱")
-//       } catch (e) {
-//           console.error("Sorry! We couldn't find one or both of the cat pictures. Make sure you encoded in BASE64!")
-//           process.exit(1)
-//       }
+        functions.validateResponseStatus(resp, uri)
 
-//   })().catch( e => { console.error("Try again! Did you install your npm packages? We got this error when trying to make a request: " + e); process.exit(1) })
-// } catch (e) {
-//     console.error("You have not added your function url as a secret!");
-//     process.exit(1)
-// }
+        if (test.length < 3) {
+            console.error("No response... Try again!")
+            process.exit(1)
+        }
+        try {
+            var catimage = test;
+            var newCat = Buffer.from(catimage, 'base64').toString('ascii')
+            console.info("Yay! 🎉 We got your cat picture 🐱")
+        } catch (e) {
+            console.error("Sorry! We couldn't find one or both of the cat pictures. Make sure you encoded in BASE64!")
+            await functions.throwError("Sorry! We couldn't find one or both of the cat pictures. Make sure you encoded in BASE64!", user, repo)
+            process.exit(1)
+        }
 
-console.log("PASSING")
+    }
+    catch (e) {
+        await functions.throwError(e, user, repo)
+    }
+}
+
+main();
