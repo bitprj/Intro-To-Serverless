038fbb2bac2b10a1cf870a60d8220f1746aeeaae
Ganning Xu <ganningxu@gmail.com>
2021-08-04 10:13:15 -0400

    Update functions.js

--- .bit/tests/functions.js
+++ .bit/tests/functions.js
@@ -2,19 +2,19 @@ const fs = require('fs') //get the methods in the fs package
 const fetch = require('node-fetch');
 
 const validateResponseStatus = (resp, uri) => {
-    if(resp.status == 404){
-        console.error(`Your function could not be found at "${uri}" check function url secret 🔍`);
+    if (resp.status == 404) {
+        throw new Error(`Your function could not be found at '${uri}' check function url secret 🔍`);
         process.exit(1)
     }
 
-    if(resp.status == 500){
-        console.error("Your function has an error and could not be run 🐛");
+    if (resp.status == 500) {
+        throw new Error("Your function has an error and could not be run 🐛");
         process.exit(1)
     }
 }
 
 const queryString = (uri) => {
-    if(uri.indexOf("?") === -1) {
+    if (uri.indexOf("?") === -1) {
         uri = uri + "?x=1"
     }
     return uri
@@ -22,17 +22,17 @@ const queryString = (uri) => {
 
 const checkSecret = (secret, secretName) => {
     if (!secret || !secret.trim()) {
-        console.error(`You forgot to add your "${secretName}" secret!`);
+        throw new Error(`You forgot to add your '${secretName}' secret!`);
         process.exit(1)
     }
 }
 
 const checkCommit = (commit_file) => {
-    for(var i = 0; i < commit_file.length; i++) {
+    for (var i = 0; i < commit_file.length; i++) {
         var a = commit_file[i];
         fs.access(commit_file[i], err => {
             if (err) {
-                console.error("You did not commit '" + a + "'")
+                throw new Error("You did not commit '" + a + "'")
                 process.exit(1)
             }
         })
