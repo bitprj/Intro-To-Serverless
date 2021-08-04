8aeaa4b8f773a483969931587bef38f259c0c8ae
Ganning Xu <ganningxu@gmail.com>
2021-08-04 10:10:18 -0400

    Update test.1.4.js

--- .bit/tests/test.1.4.js
+++ .bit/tests/test.1.4.js
@@ -1,50 +1,44 @@
-// let uri;
-// const fetch = require('node-fetch');
-// const args = require('minimist')(process.argv.slice(2))
-// const functions = require('./functions');
-// const user = args['user'];
-// const repo = args['repo'];
 
-// uri = process.env.HACKERVOICE_ENDPOINT;
+let uri;
+const fetch = require('node-fetch');
+const args = require('minimist')(process.argv.slice(2))
+const functions = require('./functions');
+const user = args['user'];
+const repo = args['repo'];
 
-// functions.checkSecret(uri, "HACKERVOICE_ENDPOINT")
+async function main() {
+    uri = process.env.HACKERVOICE_ENDPOINT;
 
-// //if you wanna add more files, just put a comma after the filename (array)
+    try {
+        functions.checkSecret(uri, "HACKERVOICE_ENDPOINT")
+        const commit_file = ['hackervoice/index.js']
+        functions.checkCommit(commit_file)
 
-// const commit_file = ['hackervoice/index.js']
-// functions.checkCommit(commit_file)
+        uri = functions.queryString(uri)
 
-// uri = functions.queryString(uri)
+        test1 = "fifiiscool";
+        uri1 = uri + "&password=" + test1;
 
-// test1 = "fifiiscool";
-// uri1 = uri + "&password=" + test1;
 
-// try {
-//     (async () => {
-//         const resp = await fetch(uri1, {
-//             method: 'GET'
-//         });
-//         var data = await resp.text()
-//         let test = JSON.stringify(data)
+        const resp = await fetch(uri1, {
+            method: 'GET'
+        });
+        var data = await resp.text()
 
-//         functions.validateResponseStatus(resp, uri)
+        functions.validateResponseStatus(resp, uri)
 
-//         if (data == test1) {     
-//             console.info("Yay! 🎉 We got: " + JSON.stringify(data) + ", which matches our input.")
-//         } else {
-//             await functions.throwError("We got this " + JSON.stringify(data) + ". We should have gotten our input, 'fifiiscool' ... Try again!", user, repo)
-//             console.error("We got this " + JSON.stringify(data) + ". We should have gotten our input, 'fifiiscool' ... Try again!")
-//             process.exit(1)
-//         }
-//     })().catch( e => { 
-//         console.error("Try again! We got this error when trying to make a request: " + e); 
-//         await functions.throwError("Try again! We got this error when trying to make a request: " + e, user, repo)
-//         process.exit(1) 
-//     })
-// } catch (e) {
-//     await functions.throwError("You have not added your function url as a secret!", user, repo)
-//     console.error("You have not added your function url as a secret!");
-//     process.exit(1)
-// }
+        if (data == test1) {
+            console.log("Yay! 🎉 We got: " + JSON.stringify(data) + ", which matches our input.")
+        } else {
+            console.error("We got this " + JSON.stringify(data) + ". We should have gotten our input, 'fifiiscool' ... Try again!")
+            await functions.throwError("We got this " + JSON.stringify(data) + ". We should have gotten our input, 'fifiiscool' ... Try again!", user, repo)
+            process.exit(1)
+        }
+    }
+    catch (e) {
+        console.error(e);
+        await functions.throwError(e, user, repo)
+    }
+}
 
-console.log("PASSING 1.4 TESTSSSSSSS")
+main();
