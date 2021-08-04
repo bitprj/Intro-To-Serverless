25386f581ba1319266a0530a18179bc49fb0c3b5
Ganning Xu <ganningxu@gmail.com>
2021-08-04 15:11:24 -0400

    Update 4.1.spec.js

--- cypress/integration/4.1.spec.js
+++ cypress/integration/4.1.spec.js
@@ -1,19 +1,3 @@
-const functions = require('../../.bit/tests/functions.js');
-const user = args['user'];
-const repo = args['repo'];
-
-Cypress.on('fail', async (error, runnable) => {
-    console.log("CUSTOM ERROR MESSSASGE")
-    console.error("CUSTOM ERROR MESSSASGE")
-
-    console.log(error)
-    // we now have access to the err instance
-    // and the mocha runnable this failed on
-    await functions.throwError(error, user, repo)
-
-    throw error // throw error to have test still fail
-})
-
 describe('Testing Bunnimage', () => {
     it('Testing Week 4 Step 1', () => {
         cy.visit('bunnimage/index.html')
