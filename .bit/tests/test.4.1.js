55ea944f2cb1bb8b97558c76ab0858646a3a9e82
Ganning Xu <ganningxu@gmail.com>
2021-08-05 11:17:24 -0400

    added dot in file path

--- .bit/tests/test.4.1.js
+++ .bit/tests/test.4.1.js
@@ -5,24 +5,17 @@ const args = require('minimist')(process.argv.slice(2))
 const user = args['user'];
 const repo = args['repo'];
 
-// cypress
-//     .run({
-//         // the path is relative to the current working directory
-//         spec: 'cypress/integration/4.1.spec.js',
-//     })
-//     .then((results) => {
-//         console.log(results)
-//     })
-//     .catch(async (err) => {
-//         console.error("ERROR: " + err);
-//         await functions.throwError(err, user, repo)
-//     });
-(async () => {
-    const results = await cypress.run({
+cypress
+    .run({
         // the path is relative to the current working directory
-        spec: 'cypress/integration/4.1.spec.js',
+        spec: './cypress/integration/4.1.spec.js',
     })
-    // use the results object
-    let data = await results.json();
-    console.log(data);
-})()
+    .then((results) => {
+        console.log(results)
+    })
+    .catch(async (err) => {
+        console.error("ERROR: " + err);
+        await functions.throwError(err, user, repo)
+        process.exit(1);
+    });
+
