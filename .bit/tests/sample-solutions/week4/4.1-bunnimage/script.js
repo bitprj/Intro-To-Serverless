bea9e8a8eb4984bd18bc650b2b8fbbefe92f86c0
emsesc <emseschen@gmail.com>
2022-03-05 21:20:58 -0500

    fixed solution code

--- .bit/tests/sample-solutions/week4/4.1-bunnimage/script.js
+++ .bit/tests/sample-solutions/week4/4.1-bunnimage/script.js
@@ -1,5 +1,5 @@
 const bunnForm = document.getElementById('bunnForm');
-bunnForm.addEventListener('submit, function (event) {
+bunnForm.addEventListener('submit', function (event) {
    event.preventDefault();
    const username = document.getElementById("username").value
    const output = document.getElementById("output")
