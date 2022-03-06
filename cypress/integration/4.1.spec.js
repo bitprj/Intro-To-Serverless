efcc2ab3d7f5cb832940ad7faebf3e05aef980d1
emsesc <emseschen@gmail.com>
2022-03-05 21:18:40 -0500

    fixed inconsistencies

--- cypress/integration/4.1.spec.js
+++ cypress/integration/4.1.spec.js
@@ -2,7 +2,7 @@ describe('Testing Bunnimage', () => {
     it('Testing Week 4 Step 1', () => {
         cy.visit('bunnimage/index.html')
         cy.get('input[type="text"]').type('console.log("hi yall")')
-        cy.get('input[type="button"]').click()
+        cy.get('input[type="submit"]').click()
         cy.get('#output').contains('console.log("hi yall")❤️')
     })
 })
