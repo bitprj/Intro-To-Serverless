ccf3d7ce2fa6ef45093d51edf73ae43b4934ea45
Christian Lechner <lechnerc77@users.noreply.github.com>
2022-04-03 09:43:14 +0200

    several fixes for consistency

--- .bit/tests/sample-solutions/week2/2.3-emotional.js
+++ .bit/tests/sample-solutions/week2/2.3-emotional.js
@@ -1,15 +1,16 @@
-var fetch = require('node-fetch');
-var multipart = require('parse-multipart')
-  
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.'); 
+const fetch = require('node-fetch');
+const multipart = require('parse-multipart');
+// npm i node-fetch@2
+// npm i parse-multipart
 
-    var boundary = multipart.getBoundary(req.headers['content-type']);
-    // parse the body
-    var parts = multipart.Parse(req.body, boundary);
-    
-    //analyze the image
-    var result = await analyzeImage(parts[0].data);
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const boundary = multipart.getBoundary(req.headers['content-type']);
+
+    const parts = multipart.Parse(req.body, boundary);
+
+    const result = await analyzeImage(parts[0].data);
 
     context.res = {
         body: {
@@ -17,30 +18,31 @@ module.exports = async function (context, req) {
         }
         // REMEMBER TO RETURN IN JSON!
     };
-    console.log(result)
-    context.done(); 
+
+    console.log(result);
+
 };
- 
-async function analyzeImage(byteArray){
-    
+
+async function analyzeImage(byteArray) {
+
     const subscriptionKey = process.env['subscriptionKey'];
     const uriBase = process.env['endpoint'] + '/face/v1.0/detect';
 
     let params = new URLSearchParams({
         'returnFaceId': 'true',
         'returnFaceAttributes': 'emotion'
-    })
+    });
 
-    let resp= await fetch(uriBase + `?${params.toString()}`, {
+    const resp = await fetch(uriBase + `?${params.toString()}`, {
         method: 'POST',
         body: byteArray,
         headers: {
             'Content-Type': "application/octet-stream",
             "Ocp-Apim-Subscription-Key": subscriptionKey
         }
-    })
+    });
 
-    let data = await resp.json();
-    
-    return data; 
-}
+    const data = await resp.json();
+
+    return data;
+};
