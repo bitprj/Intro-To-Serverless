eb93782a4c6ff0a5739ff1caa4734ebe84f48180
Christian Lechner <lechnerc77@users.noreply.github.com>
2022-04-03 09:53:40 +0200

    adjusted install comment

--- .bit/tests/sample-solutions/week2/2.3-emotional.js
+++ .bit/tests/sample-solutions/week2/2.3-emotional.js
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch');
 const multipart = require('parse-multipart');
-// npm i node-fetch@2
-// npm i parse-multipart
+//npm i node-fetch@2 parse-multipart
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
