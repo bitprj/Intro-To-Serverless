9e3919e17b0fc9bcd09453e6c06ebe73b521375b
Emily Chen <69332964+emsesc@users.noreply.github.com>
2022-04-09 21:43:16 -0400

    Update 3.4-bunnimage.js

--- .bit/tests/sample-solutions/week3/3.4-bunnimage.js
+++ .bit/tests/sample-solutions/week3/3.4-bunnimage.js
@@ -1,39 +1,33 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 const connectionstring = process.env["AZURE_STORAGE_CONNECTION_STRING"];
-const account = "bunnimagestorage";
-
-// https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-nodejs
-// 0 */5 * * * *
-// https://crontab.guru/every-5-minutes
 
 module.exports = async function (context, myTimer) {
-    // create blobserviceclient object that is used to create container client
+    var timeStamp = new Date().toISOString();
     const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionstring);
     const deletecontainer = "images";
-    const deletecontainerClient = await blobServiceClient.getContainerClient(deletecontainer);    
+    const deletecontainerClient = await blobServiceClient.getContainerClient(deletecontainer);
+
     for await (const blob of deletecontainerClient.listBlobsFlat()) {
         context.log('\t', blob.name);
-        await deleteBlob(blob.name)
+        var outputPrint = await deleteBlob(blob.name)
+        context.log(outputPrint)
+        // access the blob's name and call deleteBlob to delete it!
     }
     context.log("Just deleted your blobs!")
-
+    context.log('JavaScript timer trigger function ran!', timeStamp);   
 };
 
-async function deleteBlob(filename){
-    // create blobserviceclient object that is used to create container client
+async function deleteBlob(filename) {
     const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionstring);
     const deletecontainer = "images";
     const deletecontainerClient = await blobServiceClient.getContainerClient(deletecontainer);
     deletecontainerClient.deleteBlob(filename)
 
-    console.log(`Deleted block blob ${filename} successfully`);
     result = {
-        body : {
+        body: {
             deletename: filename,
             success: true
         }
     };
     return result;
-    // this is optional, just shows how you can check on progress - always log everything!
-
 }
