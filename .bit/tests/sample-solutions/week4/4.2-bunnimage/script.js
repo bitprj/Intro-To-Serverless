da0d4e5de09d1c53666fed01004068bc83463792
Christian Lechner <lechnerc77@users.noreply.github.com>
2022-04-23 17:14:01 +0200

    fixes for sample solution week 4 step 2

--- .bit/tests/sample-solutions/week4/4.2-bunnimage/script.js
+++ .bit/tests/sample-solutions/week4/4.2-bunnimage/script.js
@@ -1,14 +1,23 @@
 const bunnForm = document.getElementById('bunnForm');
 
 bunnForm.addEventListener('submit', function (event) {
-   event.preventDefault();
-   const payload = new FormData(bunnForm);
+    event.preventDefault();
 
-   const username = document.getElementById("username").value
-   const output = document.getElementById("output")
-   if (document.getElementById("username").value != '') {
-        output.textContent = "Thanks!"
-    } else {
-        alert("No name error.")
+    // Check the the File is a JPEG or PNG
+    const image = document.getElementById('image').files[0];
+
+    if (image.type !== 'image/jpeg' && image.type !== 'image/png') {
+        alert('Please upload a JPEG or PNG file');
+        return;
     }
+
+    // Check that the file name is supplied
+    if (document.getElementById('filename').value === '') {
+        alert('Please provide a file name.')
+        return;
+    }
+
+    const output = document.getElementById("output")
+    output.textContent = 'Thanks!'
+
 });
