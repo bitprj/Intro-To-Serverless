28b57d815a35614caf8f8397f4c4b76f7201ed19
Christian Lechner <lechnerc77@users.noreply.github.com>
2022-04-24 09:59:31 +0200

    Sample solution week 4 step 3 - fixes

--- .bit/tests/sample-solutions/week4/4.3-bunnimage/script.js
+++ .bit/tests/sample-solutions/week4/4.3-bunnimage/script.js
@@ -1,28 +1,42 @@
 const bunnForm = document.getElementById('bunnForm');
 
 bunnForm.addEventListener('submit', function (event) {
-  event.preventDefault()
-  var myform = document.getElementById("bunnForm")
-  var payload = new FormData(myform);
-  console.log(payload)
-  var username = document.getElementById("username").value;
-  const output = document.getElementById("output");
-  
-  if (username != '') {
-      output.textContent = "Thanks!"
-      console.log("Posting your image...");
-      const resp = await fetch("YOUR_URL", {
-          method: 'POST',
-          headers: {
-              'codename' : username
-          },
-          body: payload
-      });
+    event.preventDefault();
 
-      var data = await resp.text();
-      console.log(data);
-      output.textContent = "Your image has been stored successfully!"
-  } else {
-      alert("No name error.")
-  }
+    // Check the the File is a JPEG or PNG
+    const image = document.getElementById('image').files[0];
+
+    if (image.type !== 'image/jpeg' && image.type !== 'image/png') {
+        alert('Please upload a JPEG or PNG file');
+        return;
+    }
+
+    // Check that the file name is supplied
+    if (document.getElementById('filename').value === '') {
+        alert('Please provide a file name.')
+        return;
+    }
+
+    const myForm = document.getElementById("bunnForm")
+    const payload = new FormData(myForm);
+
+    console.log(payload);
+
+    const userName = document.getElementById("username").value;
+    const output = document.getElementById("output");
+
+    console.log("Posting your image...");
+
+    const resp = await fetch("YOUR_URL", {
+        method: 'POST',
+        headers: {
+            'codename': userName
+        },
+        body: payload
+    });
+
+    var data = await resp.text();
+    console.log(data);
+    output.textContent = "Your image has been stored successfully!";
+
 });
