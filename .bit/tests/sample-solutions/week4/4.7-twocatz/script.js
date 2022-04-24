737e6b6c6ad8e19d609e9ee63cb59a2e7a86ba52
Christian Lechner <lechnerc77@users.noreply.github.com>
2022-04-24 10:51:19 +0200

    Sample solution week 4 step 7 - fixes

--- .bit/tests/sample-solutions/week4/4.7-twocatz/script.js
+++ .bit/tests/sample-solutions/week4/4.7-twocatz/script.js
@@ -1,16 +1,20 @@
-let submitButton = document.getElementById("submit");
+const submitButton = document.getElementById("submit");
 
-submitButton.addEventListener("click", function(event) {
+submitButton.addEventListener("click", function (event) {
     event.preventDefault();
-    let url = `${AZURE_URL}&name1=${document.getElementById("name1").value}&name2=${document.getElementById("name2").value}&name3=${document.getElementById("name3").value}&name4=${document.getElementById("name4").value}`
-    let resp = await fetch(url, {
-            method: 'GET'
-        });
-        
-        let result = await resp.json()
-  
-        document.getElementById("image1").src = "data:image/png;base64," + result.cat1
-        document.getElementById("image2").src = "data:image/png;base64," + result.cat2
-        document.getElementById("image3").src = "data:image/png;base64," + result.cat3
-        document.getElementById("image4").src = "data:image/png;base64," + result.cat4
+
+    const AZURE_URL = "Put your Azure Function URL here";
+
+    const url = `${AZURE_URL}&name1=${document.getElementById("name1").value}&name2=${document.getElementById("name2").value}&name3=${document.getElementById("name3").value}&name4=${document.getElementById("name4").value}`
+
+    const resp = await fetch(url, {
+        method: 'GET'
+    });
+
+    const result = await resp.json()
+
+    document.getElementById("image1").src = "data:image/png;base64," + result.cat1
+    document.getElementById("image2").src = "data:image/png;base64," + result.cat2
+    document.getElementById("image3").src = "data:image/png;base64," + result.cat3
+    document.getElementById("image4").src = "data:image/png;base64," + result.cat4
 });
