910986c1d3d92b203ce004ecb323a809b92b0e63
John Kilmister <blueboxes@users.noreply.github.com>
2022-07-03 12:17:33 +0100

    Fixed formatting

--- .bit/tests/sample-solutions/week4/4.2-bunnimage/script.js
+++ .bit/tests/sample-solutions/week4/4.2-bunnimage/script.js
@@ -2,13 +2,14 @@ const bunnForm = document.getElementById('bunnForm');
 
 bunnForm.addEventListener('submit', function (event) {
    event.preventDefault();
-   const payload = new FormData(bunnForm);
-
-   const username = document.getElementById("username").value
+   
    const output = document.getElementById("output")
    if (document.getElementById("username").value != '') {
         output.textContent = "Thanks!"
+    } else if (!document.getElementById("username").value.toLowerCase().endsWith(".jpg")) {
+        alert("Invalid file type")
     } else {
-        alert("No name error.")
+        alert("No name has been given")
     }
+
 });
