b75cfe4f48800885d15c951f1589d464e62fb26a
emsesc <emseschen@gmail.com>
2022-07-27 16:14:11 -0400

    implemented suggestions

--- checkup.sh
+++ checkup.sh
@@ -1,19 +1,27 @@
 
 #!/bin/bash
-
 nodev=$(node -v);
 funcv=$(func -v);
-echo "Your node version is $nodev."
-echo "Your Azure Functions Core Tools version is $funcv."
 
+echo "🛑 Note: If you've installed node and Azure Functions Core Tools but this script is still saying you haven't, please restart your terminal and run the script again."
+echo "\n"
 echo "--------------------------------";
 echo "Checking if node is installed...";
 echo "--------------------------------";
-if which node > /dev/null
+if [[ $(node -v) ]]
 then
-    echo "Node is installed, skipping...";
+    if [[ ${nodev:1:2} -lt 14 ]]
+    then
+        echo "❗️ Your node version is $nodev, which is out of maintenance.";
+        echo "Please upgrade your node version to at least version 14.x";
+        echo "https://nodejs.org/en/download/";
+        echo "Run this script again after to continue.";
+        exit
+    else
+        echo "✅ Node $nodev is installed, skipping...";
+    fi
 else
-    echo "Node is not installed.";
+    echo "❗️ Node is not installed.";
     echo "Please install node here: https://nodejs.org/en/download/";
     echo "Run this script again after to continue.";
     exit
@@ -23,16 +31,18 @@ echo "\n"
 echo "------------------------------------------------------";
 echo "Checking if Azure Functions Core Tools is installed...";
 echo "------------------------------------------------------";
-if which func > /dev/null
+if [[ $(func -v) ]]
 then
-    if [[ ${funcv:0:1} == "2" || ${funcv:0:1} == "1" ]]
+    if [[ ${funcv:0:1} < 3 ]]
     then
-        echo "Please upgrade your Azure Functions Core Tools version to version 4.";
+        echo "❗️ Please upgrade your Azure Functions Core Tools version to version 4.";
         echo "Follow this link to learn how to: https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=v4%2Clinux%2Ccsharp%2Cportal%2Cbash#changing-core-tools-versions";
+        echo "Run this script again after to continue.";
+        exit
     fi
-    echo "Azure Function Core Tools is installed, skipping...";
+    echo "✅ Azure Function Core Tools v$funcv is installed, skipping...";
 else
-    echo "Azure Function Core Tools is not installed.";
+    echo "❌ Azure Function Core Tools is not installed.";
     echo "Please install it here: https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=v4%2Clinux%2Ccsharp%2Cportal%2Cbash#install-the-azure-functions-core-tools";
     echo "Run this script again after to continue.";
     exit
@@ -44,18 +54,14 @@ echo "Checking if versions are compatible..."
 echo "---------------------------------------"
 if [[ (${nodev:1:2} == "16" || ${nodev:1:2} == "14") && (${funcv:0:1} == "4") ]]
 then
-    echo "Compatible versions! You are good to go.";
-elif [[ (${nodev:1:2} == "14" || ${nodev:1:2} == "12" || ${nodev:1:2} == "10") && (${funcv:0:1} == "3") ]]
+    echo "✅ Compatible versions! You are good to go.";
+elif [[ (${nodev:1:2} == "14") && (${funcv:0:1} == "3") ]]
 then
-    echo "Compatible versions! You are good to go.";
-elif [[ ${nodev:1:2} == "12" ]]
-then
-    echo "Your node version is $nodev but your Core Tools version is $funcv";
-    echo "Please upgrade your node version to at least version 14.x";
-    echo "https://nodejs.org/en/download/";
+    echo "✅ Compatible versions! You are good to go.";
 elif [[ ${funcv:0:1} == "3" ]]
 then
-    echo "Your node version is $nodev but your Azure Core Tools version is $funcv";
+    echo "❗️ Your node version is $nodev but your Azure Core Tools version is $funcv";
     echo "Please upgrade your Azure Core Tools version to version 4.x";
     echo "Follow this link to learn how to: https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local?tabs=v4%2Clinux%2Ccsharp%2Cportal%2Cbash#changing-core-tools-versions";
+    echo "Run this script again after to continue.";
 fi
